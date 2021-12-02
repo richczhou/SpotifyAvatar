5c70caae95329dd6097d1fe922deaa862f65cbf8
matthewmguo <mmguo@andrew.cmu.edu>
2021-12-01 21:48:11 -0500

    library page completed

---
+++ libarary.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("myModal").style.display = "block";
+}
+  
+function closeNav() {
+    document.getElementById("myModal").style.display = "none";
+}
